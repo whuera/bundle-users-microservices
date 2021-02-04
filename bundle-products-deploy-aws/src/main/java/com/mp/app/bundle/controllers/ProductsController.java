be4e6f0b39e0bad2aeb4d5fa311d57cbbf77f1cc
whuera <whuera@gmail.com>
2021-02-04 00:39:29 -0500

    create image docker from spring

--- bundle-products-deploy-aws/src/main/java/com/mp/app/bundle/controllers/ProductsController.java
+++ bundle-products-deploy-aws/src/main/java/com/mp/app/bundle/controllers/ProductsController.java
@@ -1,4 +1,21 @@
 package com.mp.app.bundle.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class ProductsController {
+    @GetMapping
+    public String applicationstatus(){
+        return "Application is up running";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name){
+        return "Hola Bienvenido: "+name;
+    }
+
 }
