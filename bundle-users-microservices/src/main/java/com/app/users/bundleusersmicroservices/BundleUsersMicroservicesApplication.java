8ff3ac0aa3438d4e66cf9de256589c9a8ca26ed1
whuera <whuera@gmail.com>
2021-02-04 22:56:29 -0500

    first commit

---
+++ bundle-users-microservices/src/main/java/com/app/users/bundleusersmicroservices/BundleUsersMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.app.users.bundleusersmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundleUsersMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BundleUsersMicroservicesApplication.class, args);
+    }
+
+}
---
+++ bundle-users-microservices/src/main/java/com/app/users/bundleusersmicroservices/models/User.java
@@ -0,0 +1,48 @@
+package com.app.users.bundleusersmicroservices.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_address", nullable = false)
+    private String email;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    @Column(name = "created_by", nullable = false)
+    @CreatedBy
+    private String createdBy;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @Column(name = "updated_by", nullable = false)
+    @LastModifiedBy
+    private String updatedBy;
+}
